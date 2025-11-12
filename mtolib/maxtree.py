a79c0dafd5ebb25fcd1a2ab0b7dd0a11661b2708
PabloMSanAla <pablom.sanala@gmail.com>
2025-11-12 16:52:37 +0100

    update so files names suffix

--- mtolib/maxtree.py
+++ mtolib/maxtree.py
@@ -1,15 +1,20 @@
 """Build a maxtree from a numpy array."""
 
 import ctypes as ct
-import numpy.ctypeslib as npct
-import numpy as np
+import importlib.machinery
 import os
 
+import numpy as np
+import numpy.ctypeslib as npct
+
 from mtolib import _ctype_classes as mt_class
+
+__suffix__ = importlib.machinery.EXTENSION_SUFFIXES[0]
 
 
 class MaxTree:
     """A container class for the C maxtree"""
+
     def __init__(self, image, verbosity):
         self.image = image
         self.verbosity = verbosity
@@ -28,13 +33,19 @@ class MaxTree:
 
     def ctypes_maxtree(self, params):
         # Create image object
-        img_pointer = self.image.ravel().ctypes.data_as(ct.POINTER(params.d_type))
+        img_pointer = self.image.ravel().ctypes.data_as(
+            ct.POINTER(params.d_type)
+        )
 
-        return mt_class.MtData(root=self.root,
-                               nodes=self.nodes,
-                               node_attributes=self.node_attributes,
-                               img=mt_class.Image(img_pointer, *self.image.shape, self.image.size),
-                               verbosity_level = self.verbosity)
+        return mt_class.MtData(
+            root=self.root,
+            nodes=self.nodes,
+            node_attributes=self.node_attributes,
+            img=mt_class.Image(
+                img_pointer, *self.image.shape, self.image.size
+            ),
+            verbosity_level=self.verbosity,
+        )
 
 
 class OriginalMaxTree(MaxTree):
@@ -45,27 +56,43 @@ class OriginalMaxTree(MaxTree):
 
         # Get access to the compiled C maxtree library
         if params.d_type == ct.c_double:
-            self.mt_lib = ct.CDLL(os.path.join(os.path.dirname(__file__),'lib','maxtree_double.so'))
+            self.mt_lib = ct.CDLL(
+                os.path.join(
+                    os.path.dirname(__file__),
+                    "lib",
+                    "maxtree_double" + __suffix__,
+                )
+            )
         else:
-            self.mt_lib = ct.CDLL(os.path.join(os.path.dirname(__file__),'lib','maxtree.so'))
-
+            self.mt_lib = ct.CDLL(
+                os.path.join(
+                    os.path.dirname(__file__), "lib", "maxtree" + __suffix__
+                )
+            )
 
         # Create image object
         img_pointer = image.ravel().ctypes.data_as(ct.POINTER(params.d_type))
 
-        c_img = mt_class.Image(img_pointer, image.shape[0], image.shape[1], image.size)
+        c_img = mt_class.Image(
+            img_pointer, image.shape[0], image.shape[1], image.size
+        )
 
         # Create empty mt object
         self.mt = mt_class.MtData()
 
         # Set argument types for init function; Initialise max tree.
-        self.mt_lib.mt_init.argtypes = (ct.POINTER(mt_class.MtData), ct.POINTER(mt_class.Image))
+        self.mt_lib.mt_init.argtypes = (
+            ct.POINTER(mt_class.MtData),
+            ct.POINTER(mt_class.Image),
+        )
 
         self.mt_lib.mt_init(ct.byref(self.mt), ct.byref(c_img))
 
         # Set verbosity
-        self.mt_lib.mt_set_verbosity_level.argtypes = (ct.POINTER(mt_class.MtData),
-                                                  ct.c_int)
+        self.mt_lib.mt_set_verbosity_level.argtypes = (
+            ct.POINTER(mt_class.MtData),
+            ct.c_int,
+        )
         self.mt_lib.mt_set_verbosity_level(ct.byref(self.mt), verbosity)
 
     def flood(self):
